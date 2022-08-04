796854ffc529f360df8e622989bbfe428a505209
Jmajane <jordan.majane@outlook.com>
2022-08-04 12:38:48 -0400

    using variables

--- app.py
+++ app.py
@@ -1,4 +1,23 @@
 age = 31
+price = 19.95
+first_name = "Jordan"
+is_online = True
 
 print(age)
+
+
+patient_name = "John Smith"
+patient_age = 20
+new_patient = True
+patient_details = patient_name, patient_age, new_patient
+
+print(patient_details)
+
+
+
+
+
+
+
+
 
