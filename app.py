765e56b26bbba2e018da911277e52746c45fe82f
Jmajane <jordan.majane@outlook.com>
2022-08-04 14:02:26 -0400

    inputs

--- app.py
+++ app.py
@@ -1,20 +1,20 @@
-age = 31
-price = 19.95
-first_name = "Jordan"
-is_online = True
+# age = 31
+# price = 19.95
+# first_name = "Jordan"
+# is_online = True
 
-print(age)
-
-
-patient_name = "John Smith"
-patient_age = 20
-new_patient = True
-patient_details = patient_name, patient_age, new_patient
-
-print(patient_details)
+# print(age)
 
 
+# patient_name = "John Smith"
+# patient_age = 20
+# new_patient = True
+# patient_details = patient_name, patient_age, new_patient
 
+# print(patient_details)
+
+name = input("What is your name? ")
+print("Hello " + name)
 
 
 
